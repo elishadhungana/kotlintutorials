9a5f05aa2d09c183127f6191e403328c7d84fd1e
Elisha Dhungana <56481337+elishadhungana@users.noreply.github.com>
2024-04-28 14:49:53 +0100

    Update variables.kt

--- variables.kt
+++ variables.kt
@@ -2,10 +2,14 @@
 //var can be reassigned
 //val cannot be reassigned
 
+
 fun main() {
+    var number : Int = 30
+    var temp: Double =90.9
+    var isHot: Boolean=false
+    var alpha: Char = 'B'
 
-    var score = 5
-    println(score)
-    
+    println(number)
+
+
 }
-
